a1933b8e64b00add09beacf3ec28a44f42c83c99
Andy Feely <andy.feely@visma.com>
2018-12-28 00:23:24 -0700

    [master] - Trying to fix build

--- packages/vue/src/components/index.js
+++ packages/vue/src/components/index.js
@@ -1,3 +1,18 @@
-export * from './blocks';
-export * from './composites';
-export * from './structures';
+import * as blocks from './blocks';
+import * as composites from './composites';
+import * as structures from './structures';
+
+const components = {
+  ...blocks,
+  ...composites,
+  ...structures,
+};
+
+export default {
+  install: (Vue) => {
+    Object.keys(components)
+      .forEach((name) => {
+        Vue.component(name, components[name]);
+      });
+  },
+};
