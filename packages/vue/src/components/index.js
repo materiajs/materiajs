8dc836b27a5cc2602d6124a7eee37c5a5664aaa1
Andy Feely <andy.feely@visma.com>
2018-12-28 09:42:41 -0700

    [master] - Trying to fix build

--- packages/vue/src/components/index.js
+++ packages/vue/src/components/index.js
@@ -1,20 +1,27 @@
-import * as blocks from './blocks';
-import * as composites from './composites';
-import * as structures from './structures';
-
-const components = {
-  ...blocks,
-  ...composites,
-  ...structures,
-};
-
-// export default {
-//   install: (Vue) => {
-//     Object.keys(components)
-//       .forEach((name) => {
-//         Vue.component(name, components[name]);
-//       });
-//   },
+// import * as blocks from './blocks';
+// import * as composites from './composites';
+// import * as structures from './structures';
+//
+// const components = {
+//   ...blocks,
+//   ...composites,
+//   ...structures,
 // };
+//
+// // export default {
+// //   install: (Vue) => {
+// //     Object.keys(components)
+// //       .forEach((name) => {
+// //         Vue.component(name, components[name]);
+// //       });
+// //   },
+// // };
+//
+// export default components;
 
-export default components;
+export { default as TbButton } from './blocks/button/Button.vue';
+export { default as TbInput } from './blocks/input/Input.vue';
+export { default as TbList } from './blocks/list/List.vue';
+export { default as TbChip } from './blocks/chip/Chip.vue';
+export { default as TbTransitionComponent } from './blocks/transition/TransitionComponent.vue';
+export { default as TbCheckbox } from './blocks/checkbox/Checkbox.vue';
